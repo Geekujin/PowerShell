f7711752f8d07e7b532f8e73b52234812da3b77b
Geekujin <76431323+Geekujin@users.noreply.github.com>
2025-08-15 16:47:50 +0100

    Rework Get-NewAccounts.ps1
    Complete overhaul of the original script, now focused on accounts created within Azure/Entra.

--- Get-NewAccounts.ps1
+++ Get-NewAccounts.ps1
@@ -1,11 +1,81 @@
 <#
-Title: Get-NewAccounts
-Version: 1
-Author: DrGeekthumb
-Info: Returns user accounts which have been created in the past $daysback days
+.SYNOPSIS
+    Retrieves all new Entra accounts from past X days.
+
+.DESCRIPTION
+    Connects to Microsoft Entra and checks for account creation date
+	within the number of days specified.
+
+.NOTES
+    Author: Geekujin
+    Version: 2.0
 #>
 
-import-module activedirectory
+$scriptTitle = "New Entra accounts checker" 
 
-$daysback = read-host "Go back how many days?"
-Get-ADUser -Filter * -Property whenCreated | Where {$_.whenCreated -gt (Get-Date).AddDays(-$daysback)} | FT Name, whenCreated -Autosize
+Clear-Host
+Write-Host ""
+Write-Host " "$scriptTitle
+Write-Host " "("=" * $scriptTitle.Length)
+Write-Host " "
+$daysBack = Read-Host "Enter number of days to go back"
+Write-Host " "
+
+$startDate = (Get-Date).AddDays(-$daysBack).ToString("yyyy-MM-ddTHH:mm:ssZ")
+
+$users = Get-MgUser -Filter "createdDateTime ge $startDate" -All -Property DisplayName, JobTitle, AccountEnabled, UserType
+
+# SET COLUMN WIDTHS
+$displayNameWidth = 35
+$jobTitleWidth = 30
+$enabledWidth = 7
+$typeWidth = 8
+
+Clear-Host
+Write-Host "Retrieving Entra accounts created in past $daysBack days."
+Write-Host ""
+$header = "{0,-$displayNameWidth} {1,-$jobTitleWidth} {2,-$enabledWidth} {3,-$typeWidth}" -f "Name", "Job Title", "Enabled", "Type"
+Write-Host $header -ForegroundColor White
+
+$separator = "{0,-$displayNameWidth} {1,-$jobTitleWidth} {2,-$enabledWidth} {3,-$typeWidth}" -f ('='*$displayNameWidth), ('='*$jobTitleWidth), ('='*$enabledWidth), ('='*$typeWidth)
+Write-Host $separator -ForegroundColor Yellow
+
+$users | ForEach-Object {
+    $foreColor = "White"
+	$backColor = $Host.UI.RawUI.BackgroundColor
+	
+    if ($_.UserType -eq 'Guest') {
+        $foreColor = "White"
+		$backColor = "DarkRed"
+    }
+
+	if ($_.AccountEnabled -eq $false) {
+		$foreColor = "White"
+		$backColor = "DarkGray"
+	}
+
+	# TRUNCATE TEXT IF BIGGER THAN COLUMN WIDTH
+    $displayName = $_.DisplayName
+    if ($displayName.Length -gt $displayNameWidth) {
+        $displayName = $displayName.Substring(0, $displayNameWidth - 3) + "..."
+    }
+
+
+    $jobTitle = $_.JobTitle
+    if ($jobTitle -and $jobTitle.Length -gt $jobTitleWidth) {
+        $jobTitle = $jobTitle.Substring(0, $jobTitleWidth - 3) + "..."
+    }
+
+	
+    $output = "{0,-$displayNameWidth} {1,-$jobTitleWidth} {2,-$enabledWidth} {3,-$typeWidth}" -f $displayName, $jobTitle, $_.AccountEnabled, $_.UserType
+    
+    Write-Host $output -ForegroundColor $foreColor -BackgroundColor $backColor
+}
+Write-Host ""
+Write-Host "Key:`n"
+Write-Host "  " -BackgroundColor DarkGray -NoNewLine
+Write-Host ": Disabled accounts." -NoNewLine
+Write-Host " " -NoNewLine
+Write-Host "  " -BackgroundColor DarkRed -NoNewLine
+Write-Host ": Guest accounts."
+Write-Host "`n"
